9fc59505bb8ac8f1eec5e81039421e9f9cdd1257
Elicarlos <elicarlosantos_@hotmail.com>
2023-09-29 16:35:38 -0300

    ajustes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import Aluno, AnoLetivo, Aula, Disciplina, Matricula, Professor, Serie
 
 # Register your models here.
+admin.site.register(Aluno)
+admin.site.register(AnoLetivo)
+admin.site.register(Aula)
+admin.site.register(Disciplina)
+admin.site.register(Matricula)
+admin.site.register(Professor)
+admin.site.register(Serie)
+
--- core/models.py
+++ core/models.py
@@ -1,61 +1,87 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Create your models here.
-class Cargo(models.Model):
-    pass
-
-class Turno(models.Model):
-    pass
-
-class Turma(models.Model):
-    pass
-
-
-class Disciplina(models.Model):
-    pass
-
 
 class Aluno(models.Model):
-    CHOICE_SEXO = (('M', 'Masculino'), ('F', 'Feminino'))
-    CHOICE_TURNO = (('M', 'Manhã'), ('T', 'Tarde'), ('N', 'Noite'))
-    CHOICE_TURMA = (('1', '1-A'), ('2', '1-B'), ('3', '1-C'),('4', '2-A'), ('5', '2-B'), ('6', '2-C'),('7', '3-A'), ('8', '3-B'), ('9', '3-C'))
     nome = models.CharField(max_length=100)
-    idade = models.CharField(max_length=10)
-    data_de_nascimento = models.DateField()
-    sexo = models.CharField(max_length=11, choices=CHOICE_SEXO, blank=True) 
-    turno = models.CharField(max_length=11, choices=CHOICE_TURNO, blank=True) 
-    turma = models.CharField(max_length=11, choices=CHOICE_TURMA, blank=True) 
-    email = models.EmailField()
-    telefone = models.CharField(max_length=11)
-    foto = models.ImageField()
-    
+    data_nascimento = models.DateField()
+    contato = models.CharField(max_length=100)
+    endereco = models.TextField()
+
     def __str__(self):
         return self.nome
     
 
-class Funcionario(models.Model):
-    CHOICE_SEXO = (('M', 'Masculino'), ('F', 'Feminino'))
-    CHOICE_TURNO = (('M', 'Manhã'), ('T', 'Tarde'), ('N', 'Noite'))
-    CHOICE_DICIPLINA = (('1', 'português'), ('2', 'matematica'), ('3', 'historia'),('4', 'geografia'), ('5', 'fisica'), ('6', 'biologia'),('7', 'ed:fisica'), ('8', 'informatica'), ('9', 'ingles'))
+class Professor(models.Model):
     nome = models.CharField(max_length=100)
-    idade = models.CharField(max_length=10)
-    data_de_nascimento = models.DateField()
-    sexo = models.CharField(max_length=11, choices=CHOICE_SEXO, blank=True) 
-    turno = models.CharField(max_length=11, choices=CHOICE_TURNO, blank=True) 
-    diciplina = models.CharField(max_length=11, choices=CHOICE_DICIPLINA, blank=True) 
-    email = models.EmailField()
-    telefone = models.CharField(max_length=11)
-    foto = models.ImageField()
+    qualificacoes = models.TextField()
+    contato = models.CharField(max_length=100)
+    horario_disponivel = models.TextField()
+
+    def __str__(self):
+        return self.nome
     
-    
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
     def __str__(self):
         return self.nome
     
 
+
+class Serie(models.Model):
+    nome = models.CharField(max_length=100)
+    ano_letivo = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+    
+class AnoLetivo(models.Model):
+    ano = models.PositiveIntegerField(unique=True)
+    inicio = models.DateField()
+    fim = models.DateField()
+
+    def __str__(self):
+        return f"Ano Letivo {self.ano}"
+
+    class Meta:
+        ordering = ['-ano']
     
 class Matricula(models.Model):
-    pass
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    serie = models.ForeignKey(Serie, on_delete=models.CASCADE)
+    ano_letivo = models.ForeignKey(AnoLetivo, on_delete=models.CASCADE)
+    data_matricula = models.DateField(auto_now_add=True)
+    matricula = models.CharField(max_length=20, unique=True, blank=True)
+    ativa = models.BooleanField(True)
+
+    class Meta:
+        unique_together = ('aluno', 'serie', 'ano_letivo')
+
+    def __str__(self):
+        return f"Matrícula de {self.aluno} na {self.serie} ({self.ano_letivo})"
+    
+    def gerar_matricula(cls, aluno, serie, ano_letivo):
+        codigo_aluno = aluno.id 
+        matricula = f"{ano_letivo}{serie.id:02d}{codigo_aluno:04d}"
+        return matricula
+    
+    def save(self, *args, **kwargs):
+        if not self.matricula:
+            self.matricula = self.gerar_matricula(self.aluno, self.serie, self.ano_letivo)
+
+
+
+
+class Aula(models.Model):
+    data = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_fim = models.TimeField()
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    serie = models.ForeignKey(Serie, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    alunos = models.ManyToManyField(Aluno, blank=True)
+
+    def __str__(self):
+        return f"Aula de {self.disciplina} na {self.serie} - {self.data} ({self.hora_inicio}-{self.hora_fim})"
 
